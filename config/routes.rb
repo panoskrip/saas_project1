4cfdeadfb18ca3c15630d13aa617eac53a727d9e
pkrip <p.criparacos@gmail.com>
2024-02-12 16:38:15 +0200

    changed login route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
